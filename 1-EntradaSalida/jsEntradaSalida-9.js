f9d451d06f0f127abd5cbeebb988090e239ce33e
alumno <alumno>
2019-02-06 20:42:49 -0300

    Ej 9 bis

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,7 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-	var vSueldo=sueldo.value;
-	vSueldo=parseInt(vSueldo);
-	resultado.value=(vSueldo*1.1);
+	var vSueldo;
+	var aumento;
+
+	vSueldo=sueldo.value;
+	//vSueldo=parseInt(vSueldo); no es necesario
+	//resultado.value=(vSueldo*1.1);
+	
+	aumento=vSueldo*1.1;
+
+	resultado.value=aumento;
 }
