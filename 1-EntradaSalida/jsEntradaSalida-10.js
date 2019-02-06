5dddd5138171404b247834a1c40b500353ae6c99
alumno <alumno>
2019-02-06 20:50:21 -0300

    Ej 10 bis

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,7 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	var vImporte=importe.value;
+	/*var vImporte=importe.value;
 	vImporte=parseInt(vImporte);
-	resultado.value=(vImporte*0.75);
+	resultado.value=(vImporte*0.75);*/
+
+	var primerSueldo;
+	var descuento;
+	var nuevoSueldo;
+
+	primerSueldo=importe.value;
+	descuento=0.75;
+
+	nuevoSueldo=primerSueldo*descuento;
+
+	resultado.value=nuevoSueldo;
 }
