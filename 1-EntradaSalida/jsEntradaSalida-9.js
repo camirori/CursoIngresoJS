9087b4b0eb313f5efdbd9daa00aadc69f37c588f
alumno <alumno>
2019-02-06 21:45:29 -0300

    Ej 1.9 v2

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,13 +5,13 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 	var vSueldo;
-	var aumento;
+	var nuevoSueldo;
 
 	vSueldo=sueldo.value;
 	//vSueldo=parseInt(vSueldo); no es necesario
 	//resultado.value=(vSueldo*1.1);
 	
-	aumento=vSueldo*1.1;
+	nuevoSueldo=vSueldo*1.1;
 
-	resultado.value=aumento;
+	resultado.value=nuevoSueldo;
 }
