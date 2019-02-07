08f3f43abf6c1766c3490484026b17837176dfb2
alumno <alumno>
2019-02-06 21:54:27 -0300

    Ej 2.6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+	var edadIngresada;
+	edadIngresada=edad.value;
+	//vEdad=parseInt(vEdad); no es necesario
 
 
-
+	if(edadIngresada>=18)
+	{	
+		alert("Usted es mayor de edad");
+	}
+	else if(edadIngresada>=13&&edadIngresada<=17)
+	{
+		alert("Usted es adolescente");	
+	}
+	else
+	{	
+		alert("Usted es un niño");
+	}
 
 }//FIN DE LA FUNCIÓN
