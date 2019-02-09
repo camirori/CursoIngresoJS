ee3d0910c1efa5c58e1416e0f98ff11493bdb02e
camirori <you@example.com>
2019-02-08 22:48:33 -0300

    ParcialRec Ej1 v1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
 
+	largo=prompt("Ingresar largo");
+	ancho=prompt("Ingresar ancho");
+
+	perimetro=largo*2+ancho*2;
+
+	alert("El perimetro es "+perimetro);
 }
