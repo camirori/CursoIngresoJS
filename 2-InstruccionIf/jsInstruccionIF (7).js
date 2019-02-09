702b2db6f2c836ee988cc21763d574ae9758f77d
camirori <you@example.com>
2019-02-08 21:52:28 -0300

    Ej 2.7 v1

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+	var edadIngresada;
+	var estado;
+	edadIngresada=edad.value;
+	estado=estadoCivil.value;
 
+	edadIngresada=parseInt(edadIngresada);
+
+	if(edadIngresada<18&&estado!="Soltero")
+		{alert("Usted es muy pequeño para NO ser soltero");}
 	
-
 
 }//FIN DE LA FUNCIÓN
