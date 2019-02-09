0cc09e1a477b8b6d09e2ae4dc9903bb37a58ff79
camirori <you@example.com>
2019-02-08 22:33:40 -0300

    Parcial Ej3 v1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+	var precioIngresado;
+	var precioIngresado;
+	var precioFinal;
 
+	precioIngresado=prompt("Ingrese el precio");
+	precioIngresado=prompt("Ingrese el porcentaje de descuento")
+
+	precioFinal=precioIngresado*(1-precioIngresado/100)
+
+	elPrecioFinal.value="El precio final con descuento es "+precioFinal;
 }
