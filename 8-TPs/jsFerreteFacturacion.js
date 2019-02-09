ea1c1ce4133c04e648c2fb2c409d98f9a6656f36
camirori <you@example.com>
2019-02-08 23:20:40 -0300

    TP Ej1 v1

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,61 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+
+	var precioPrimero;
+	var precioSegundo;
+	var precioTercero;
+	var calculo;
+
+	precioPrimero=PrecioUno.value;
+	precioSegundo=PrecioDos.value;
+	precioTercero=PrecioTres.value;
+
+	precioPrimero=parseInt(precioPrimero);
+	precioSegundo=parseInt(precioSegundo);
+	precioTercero=parseInt(precioTercero);
+
+	calculo=precioPrimero+precioSegundo+precioTercero;
+
+	alert("La suma es "+calculo);
+
 }
 function Promedio () 
 {
-	
+	var precioPrimero;
+	var precioSegundo;
+	var precioTercero;
+	var calculo;
+
+	precioPrimero=PrecioUno.value;
+	precioSegundo=PrecioDos.value;
+	precioTercero=PrecioTres.value;
+
+	precioPrimero=parseInt(precioPrimero);
+	precioSegundo=parseInt(precioSegundo);
+	precioTercero=parseInt(precioTercero);
+
+	calculo=(precioPrimero+precioSegundo+precioTercero)/3;
+
+	alert("El promedio es "+calculo);
+
 }
 function PrecioFinal () 
 {
-	
+	var precioPrimero;
+	var precioSegundo;
+	var precioTercero;
+	var calculo;
+
+	precioPrimero=PrecioUno.value;
+	precioSegundo=PrecioDos.value;
+	precioTercero=PrecioTres.value;
+
+	precioPrimero=parseInt(precioPrimero);
+	precioSegundo=parseInt(precioSegundo);
+	precioTercero=parseInt(precioTercero);
+
+	calculo=(precioPrimero+precioSegundo+precioTercero)*1.21;
+
+	alert("El precio final es "+calculo);
 }
