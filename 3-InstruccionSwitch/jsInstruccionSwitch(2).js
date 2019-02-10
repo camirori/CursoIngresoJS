6a69a8819daf1ef2ea22f341a5a973a1ac9f6efe
camirori <you@example.com>
2019-02-10 19:24:04 -0300

    Ej 3.2 v1

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,31 @@
 function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+{ 
+	var mesDelAño = document.getElementById('mes').value;
+	//alert (mesDelAño);
 
-alert (mesDelAño);
+	switch (mesDelAño)
+	{
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace frio.");
+			break;
+		
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno.")
+			break;
+
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+			alert("Ya pasamos el frio, ahora calor!!!.")		
 
 
-
+	}
 
 }//FIN DE LA FUNCIÓN
