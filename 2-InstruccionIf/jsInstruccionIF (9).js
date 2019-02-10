bdac6b028856af92573824ff3e1ddc089f09962f
camirori <you@example.com>
2019-02-09 23:35:13 -0300

    Ej 2.9 v1

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var aleatorio;
+
+	aleatorio=Math.random()*11;
+	aleatorio=Math.round(aleatorio);
 	
+	alert(aleatorio);
 
 }//FIN DE LA FUNCIÓN
