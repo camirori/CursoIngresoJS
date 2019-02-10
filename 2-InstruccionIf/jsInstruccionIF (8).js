1e75ca680ba004c22b1b3d474165974fd9d9f2d3
camirori <you@example.com>
2019-02-09 23:22:36 -0300

    Ej 2.8 v1

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+	var edadIngresada;
+	var estadoIngresado;
 
-	
+	edadIngresada=edad.value;
+	estadoIngresado=estadoCivil.value;
+
+	if(edadIngresada>=18&&estadoIngresado=="Soltero")
+	{
+		alert("Es soltero y no es menor.");
+	}
 
 
 }//FIN DE LA FUNCIÓN
