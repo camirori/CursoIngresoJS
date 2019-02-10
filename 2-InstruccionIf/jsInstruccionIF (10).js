65ff8cb73b2cf052837b587462aba20cf729a681
camirori <you@example.com>
2019-02-09 23:50:38 -0300

    Ej 2.10 v1

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,22 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var aleatorio;
+
+	aleatorio=Math.random()*11;
+	aleatorio=Math.round(aleatorio);
 	
+	if(aleatorio>=9)
+	{
+		alert("EXCELENTE");
+	}
+	else if(aleatorio>=4)
+	{
+		alert("APROBÓ");
+	}
+	else
+	{
+		alert("Vamos, la proxima se puede");
+	}
 
 }//FIN DE LA FUNCIÓN
