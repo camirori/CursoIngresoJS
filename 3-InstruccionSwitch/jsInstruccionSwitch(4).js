39aa848de977d2ac5af54efac1556b8c5b466672
camirori <you@example.com>
2019-02-10 19:40:49 -0300

    Ej 3.4 v1

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,32 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+		switch(mesDelAño)
+		{
+			case "Enero":
+			case "Marzo":
+			case "Mayo":
+			case "Julio":
+			case "Agosto":
+			case "Octubre":
+			case "Diciembre":
+
+				alert("El mes tiene 31 días")
+				break;
+
+			case "Abril":
+			case "Junio":
+			case "Septiembre":
+			case "Noviembre":
+				alert("El mes tiene 30 días")
+				break;
+
+			case "Febrero":
+				alert("El mes tiene 28 días")
+				break;
+
+		}
 	
 	
 
