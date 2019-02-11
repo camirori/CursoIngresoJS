5711cc9be0662b6f222cf568ae92ea5f2e2acffc
alumno <alumno>
2019-02-11 20:56:50 -0300

    Ej 5.3 v1

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+	var clave = prompt("ingrese el número clave.");
+
+	while(clave!="utn750")
+	{
+		alert("La clave es incorrecta");
+		clave = prompt("ingrese el número clave.");
+	}
 
 
 }//FIN DE LA FUNCIÓN
