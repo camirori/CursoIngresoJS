e40805966e5c5aed26426998a9986fadfcc58c08
camirori <you@example.com>
2019-02-12 23:44:51 -0300

    ParcialRec Ej2 v1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
-  
+
+  	var nombreIngresado;
+  	var localidadIngresada;
+
+  	nombreIngresado=elNombre.value;
+  	localidadIngresada=laLocalidad.value;
+
+  	alert("Usted es "+nombreIngresado+" y vive en la localidad de "+localidadIngresada);
+  	
+
 }
