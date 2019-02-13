f7a14a132b0533584d4ae3d296df175a3761702a
camirori <you@example.com>
2019-02-13 00:01:22 -0300

    ParcialRec Ej3 v1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+	var precioIngresado;
+	var porcentajeIngresado;
+	var descuento;
+	var precioConDescuento;
+	var IVA;
+	var precioFinal;
 
+	precioIngresado=prompt("Ingrese el precio");
+	porcentajeIngresado=prompt("Ingrese el porcentaje de descuento");
+
+	precioIngresado=parseInt(precioIngresado);
+	porcentajeIngresado=parseInt(porcentajeIngresado);
+
+	descuento=precioIngresado*(porcentajeIngresado/100);
+	//precioConDescuento=precioIngresado*(1-porcentajeIngresado/100);
+	precioConDescuento=precioIngresado-descuento;
+	IVA=precioConDescuento*0.21;
+	precioFinal=precioConDescuento+IVA;
+
+	alert("Descuento: $"+descuento+", precio con descuento: $"+precioConDescuento+", IVA: $"+IVA);
+	elPrecioFinal.value="El precio final con descuento es "+precioFinal;
 }
