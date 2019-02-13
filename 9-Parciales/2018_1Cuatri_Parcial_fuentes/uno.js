30bc467295478ff6fb20029f59bf5b77f3c89196
alumno <alumno>
2019-02-13 19:18:19 -0300

    Ejercicio clase 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,7 +1,7 @@
 
 function mostrar()
 {
-	var ancho;
+	/*var ancho;
 	var largo;
 	var perimetro;
 
@@ -13,6 +13,24 @@ function mostrar()
 
 	perimetro= ancho*2+largo*2;
 
-	alert("El perímetro es "+perimetro);
+	alert("El perímetro es "+perimetro);*/
+
+	//Ejercicio clase
+	var nombreIngresadoUno;
+	var nombreIngresadoDos;
+	var edadIngresadaUno;
+	var edadIngresadaDos;
+	var suma;
+
+	nombreIngresadoUno=prompt("Ingrese el nombre de la primer persona");
+	edadIngresadaUno=prompt("Ingrese la edad de la primer persona");
+	nombreIngresadoDos=prompt("Ingrese el nombre de la segunda persona");
+	edadIngresadaDos=prompt("Ingrese la edad de la segunda persona");
+
+	edadIngresadaUno=parseInt(edadIngresadaUno);
+	edadIngresadaDos=parseInt(edadIngresadaDos);
+	suma=edadIngresadaUno+edadIngresadaDos;
+
+	alert("Ustedes son "+nombreIngresadoUno+" y "+nombreIngresadoDos+", sus edades son "+edadIngresadaUno+" y "+edadIngresadaDos+" y la suma de sus edades es "+suma);
 
 }
