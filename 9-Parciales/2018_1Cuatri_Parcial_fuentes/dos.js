99c654bc77045712a93638cc8e49944ca929ddcf
alumno <alumno>
2019-02-13 19:28:51 -0300

    Ejercicio clase 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,11 +1,28 @@
 function mostrar()
 {
-  	var nombreIngresado;
+  	/*var nombreIngresado;
   	var localidadIngresada;
 
   	nombreIngresado=elNombre.value;
   	localidadIngresada=laLocalidad.value;
 
-  	alert("Usted es "+nombreIngresado+" y vive en la localidad de "+localidadIngresada);
+  	alert("Usted es "+nombreIngresado+" y vive en la localidad de "+localidadIngresada);*/
   	
+  	//Ejercicio clase
+
+  	var precioIngresado;
+  	var recargoIngresado;
+  	var recargo;
+  	var precioFinal;
+
+  	precioIngresado=prompt("Ingrese el precio");
+  	recargoIngresado=prompt("Ingrese el recargo por tarjeta");
+  	precioIngresado=parseInt(precioIngresado);
+  	recargoIngresado=parseInt(recargoIngresado);
+
+  	recargo=precioIngresado*(recargoIngresado/100);
+  	precioFinal=precioIngresado+recargo;
+
+  	alert("El precio del producto en efectivo es: $"+precioIngresado+", con tarjeta tiene un recargo del "+recargoIngresado+"% que seria $"+recargo+" de recargo, costandole final $"+precioFinal);
+
 }
