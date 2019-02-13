5d371053526ea251054676233912523f8ec778db
camirori <you@example.com>
2019-02-12 23:18:15 -0300

    Parcial Ej1 v2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -6,7 +6,10 @@ function mostrar()
 	var perimetro;
 
 	ancho=prompt("Ingrese el ancho");
-	largo=prompt("Ingrese el largo")
+	largo=prompt("Ingrese el largo");
+
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
 
 	perimetro= ancho*2+largo*2;
 
