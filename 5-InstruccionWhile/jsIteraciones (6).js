e8bfa81937e2910b8e2b88e994c844906079f008
alumno <alumno>
2019-02-13 21:14:12 -0300

    Ej 5.6 v1

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,19 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numeroIngresado;
+	
 
+	while(contador<5)
+	{
+		numeroIngresado=prompt("Indicar un nùmero");
+		numeroIngresado=parseInt(numeroIngresado);
+		acumulador=acumulador+numeroIngresado;
+		contador++
+	}
 
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
