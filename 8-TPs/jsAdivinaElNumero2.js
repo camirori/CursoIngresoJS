b985b0795e1e5739f5127a3c8d4ce490f7b20bad
camirori <you@example.com>
2019-02-16 00:23:45 -0300

    TP Ej6 v1

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -10,19 +10,81 @@ Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
+var numeroIngresado;
 var contadorIntentos;
+
+
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-	
-
+	numeroSecreto=Math.floor(Math.random()*100)+1;
+	console.log(numeroSecreto);
+	contadorIntentos=0;
 }
 
 function verificar()
 {
-	
-	
+	numeroIngresado=numero.value;
+	numeroIngresado=parseInt(numeroIngresado);
+
+	contadorIntentos++
+	intentos.value=contadorIntentos;
+
+	if(numeroIngresado==numeroSecreto)
+	{
+		alert("Usted es un ganador!!! y en solo "+contadorIntentos+" intentos");
+		switch(contadorIntentos)
+		{
+			case 1:
+			{
+				alert("usted es un Psíquico");
+				break;
+			}
+			case 2:
+			{
+				alert("excelente percepción");
+				break;
+			}
+			case 3:
+			{
+				alert("Esto es suerte");
+				break;
+			}
+			case 4:
+			{
+				alert("Excelente técnica");
+				break;
+			}
+			case 5:
+			{
+				alert("usted está en la media");
+				break;
+			}
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			{
+				alert("falta técnica");
+				break;
+			}
+			default:
+			{
+				alert("afortunado en el amor!!");
+				break;
+			}
+		}
+	}
+	else if (numeroIngresado<numeroSecreto)
+	{
+		alert("falta...");
+	}
+	else
+	{
+		alert("se pasó...");
+	}
+
 
 }
