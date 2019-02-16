ae99daccdf2ce99b4630c78cb8ebe0a383190632
camirori <you@example.com>
2019-02-15 23:37:53 -0300

    TP Ej4 v1

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,87 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+ 	var precio=35;
+ 	var cantidad;
+ 	var marca;
+ 	var importe;
+
+ 	cantidad=Cantidad.value;
+ 	cantidad=parseInt(cantidad);
+
+ 	marca=Marca.value;
+
+ 	switch (cantidad)
+ 	{
+ 		case 5:
+ 		{
+ 			if(marca=="ArgentinaLuz")
+ 			{
+ 				importe=precio*cantidad*0.6;
+ 				break;
+ 			}
+ 			else
+ 			{
+ 				importe=precio*cantidad*0.7;
+ 				break;
+ 			}
+ 		}
+
+ 		case 4:
+ 		{
+ 			if(marca=="ArgentinaLuz"||marca=="FelipeLamparas")
+ 			{
+ 				importe=precio*cantidad*0.75;
+ 				break;
+ 			}
+ 			else
+ 			{
+ 				importe=precio*cantidad*0.8;
+ 				break;
+ 			}
+ 		}
+
+ 		case 3:
+ 		{
+ 			if(marca=="ArgentinaLuz")
+ 			{
+ 				importe=precio*cantidad*0.85;
+ 				break;
+ 			}
+ 			else if(marca=="FelipeLamparas")
+ 			{
+ 				importe=precio*cantidad*0.9;
+ 				break;
+ 			}
+ 			else
+ 			{
+ 				importe=precio*cantidad*0.95;
+ 				break;
+ 			}
+ 		}
+ 		case 1:
+ 		case 2:
+ 		{
+ 			importe=precio*cantidad;
+ 			break;
+ 		}
+ 		default: //6 o más
+ 		{
+ 			importe=precio*cantidad*0.5;
+ 			break;
+ 		}
+ 	}
+
+
+
+ 	if (importe>120)
+ 	{
+ 		IIBB=importe*0.1;
+ 		importe+=IIBB;
+ 		alert("Usted pago $"+IIBB+" de IIBB.");
+ 	}
+
+ 	precioDescuento.value=importe;
+
+
 }
