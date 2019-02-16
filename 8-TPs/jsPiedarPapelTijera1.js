a1fbcf6b49ad6cda78b3abf78e6c8c13f8970dcc
camirori <you@example.com>
2019-02-16 18:13:19 -0300

    TP Ej7 v1

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,65 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var numero;
 
 function comenzar()
 {
 	
 
+	numero=Math.floor(Math.random()*3)+1;
+	console.log(numero);
+
+	/* 1=piedra
+		2=papel
+		3=tijera*/
 
 }//FIN DE LA FUNCIÓN
+
 function piedra()
 {
-	
+	if(numero==1)
+	{
+		alert("Empató");
+	}
+	else if (numero==2)
+	{
+		alert("Perdió");
+	}
+	else
+	{
+		alert("Ganó!!");
+	}
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
+	if(numero==1)
+	{
+		alert("Ganó!!");
+	}
+	else if (numero==2)
+	{
+		alert("Empató");
+	}
+	else
+	{
+		alert("Perdió");
+	}
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
-
+	if(numero==1)
+	{
+		alert("Perdió");
+	}
+	else if (numero==2)
+	{
+		alert("Ganó!!");
+	}
+	else
+	{
+		alert("Empató");
+	}
 }//FIN DE LA FUNCIÓN
