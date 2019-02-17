0d72ddc2b4244cbe191550917669fdbe208ba4c5
camirori <you@example.com>
2019-02-17 01:29:17 -0300

    Parcial Ej6 v1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,59 @@
 function mostrar()
 {
 
+	var horaIngresada;
+
+	horaIngresada=laHora.value;
+	horaIngresada=parseInt(horaIngresada);
+
+	switch(horaIngresada)
+	{
+
+
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		{
+			alert("Es de mañana");
+			break;
+		}
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		{
+			alert("Es de tarde");
+			break;
+		}
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+		case 24:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		{
+			alert("Es de noche");
+			if(horaIngresada<24&&horaIngresada>19)
+			{
+				alert("A dormir");
+			}
+			break;
+		}	
+		default:
+		{
+			alert("La hora no es válida");
+			break;
+		}
+	}
 }
