c0cb72795b9946426e3421dd9a09b66bbdb5777f
camirori <you@example.com>
2019-02-17 18:51:15 -0300

    ParcialRec Ej6 v1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,4 +1,57 @@
 function mostrar()
 {
+	var notaIngresada;
+
+	notaIngresada=laHora.value;
+	notaIngresada=parseInt(notaIngresada);
+
+	switch(notaIngresada)
+	{
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		{
+			alert("La próxima se puede");
+			break;
+		}
+		case 4:
+		case 5:
+		case 6:
+		{
+			if(notaIngresada<5)
+			{
+				alert("Raspando. Debes preocuparte más");
+			}
+			else
+			{
+				alert("Raspando");
+			}
+			break;
+		}
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		{
+			if(notaIngresada>8)
+			{
+				alert("Aprobó. Muy bien.");
+			}
+			else
+			{
+				alert("Aprobó");
+			}
+			break;
+		}
+		default:
+		{
+			alert("La nota no es válida");
+			break;
+		}
+
+
+	}
+
 
 }
