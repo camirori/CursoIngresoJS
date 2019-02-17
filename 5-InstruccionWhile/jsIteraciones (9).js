10329f0107f8b06c022c65aa270365f0e9cfad94
camirori <you@example.com>
2019-02-17 19:45:52 -0300

    Ej 5.9 v1

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,34 @@ function mostrar()
 {
 
 	var contador=0;
+	var numeroIngresado;
+	var maximoIngresado;
+	var minimoIngresado;
+
 	// declarar variables
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
+		numeroIngresado=prompt("Ingrese un número");
+		numeroIngresado=parseInt(numeroIngresado);
+
+		respuesta=prompt("Desea ingresar otro número?");
+
+		if(numeroIngresado>maximoIngresado||maximoIngresado==undefined)
+		{
+			maximoIngresado=numeroIngresado;
+		}
+		if(numeroIngresado<minimoIngresado||minimoIngresado==undefined)
+		{
+			minimoIngresado=numeroIngresado;
+		}
 	
 	}
 
-
+	maximo.value=maximoIngresado;
+	minimo.value=minimoIngresado;
 
 
 }//FIN DE LA FUNCIÓN
