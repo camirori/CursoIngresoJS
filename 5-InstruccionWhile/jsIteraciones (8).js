a0b7c2f63037b64ce515cc41f89516117b57ca86
camirori <you@example.com>
2019-02-17 19:12:00 -0300

    Ej 5.8 v1

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,26 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numeroIngresado;
 	
 	var respuesta='si';
+
+	while(respuesta=="si")
+	{
+		numeroIngresado=prompt("Ingrese un número");
+		numeroIngresado=parseInt(numeroIngresado);
+
+		respuesta=prompt("Desea ingresar otro número?");
+
+		if(numeroIngresado>=0)
+		{
+			positivo+=numeroIngresado;
+		}
+		else
+		{
+			negativo*=numeroIngresado;
+		}
+	}
 
 
 document.getElementById('suma').value=positivo;
