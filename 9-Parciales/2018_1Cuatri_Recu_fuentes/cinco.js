74446aaec07ea64c8575c5e2fa3454383071b190
camirori <you@example.com>
2019-02-17 18:39:58 -0300

    ParcialRec Ej5 v1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
+	var diaIngresado;
 
+	diaIngresado=prompt("Ingrese un día de la semana");
+
+	switch(diaIngresado)
+	{
+		case "lunes":
+		case "martes":
+		case "miercoles":
+		case "jueves":
+		case "viernes":
+		{
+			alert("A trabajar");
+			break;
+		}
+
+		case "sabado":
+		case "domingo":
+		{
+			alert("Buen finde");
+			break;
+		}
+		default:
+		{
+			alert("El día no es válido");
+			break;
+		}
+
+	}
 }
