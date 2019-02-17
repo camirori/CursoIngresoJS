762e3a10816a3942ff12a05e1f74f581c89f5a23
camirori <you@example.com>
2019-02-17 18:33:50 -0300

    ParcialRec Ej4 v1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
 
+	var respuesta;
+	var primerNumeroIngresado;
+	var segundoNumeroIngresado;
+
+	primerNumeroIngresado=prompt("Ingrese un número");
+	segundoNumeroIngresado=prompt("Ingrese otro número");
+
+	primerNumeroIngresado=parseInt(primerNumeroIngresado);
+	segundoNumeroIngresado=parseInt(segundoNumeroIngresado);
+
+	if(primerNumeroIngresado==segundoNumeroIngresado)	
+	{
+		alert("Los números ingresados son "+primerNumeroIngresado+" y "+segundoNumeroIngresado);
+	}
+	else if(primerNumeroIngresado>segundoNumeroIngresado)
+	{
+		respuesta=primerNumeroIngresado/segundoNumeroIngresado;
+		alert("La division es "+respuesta);
+	}
+	else
+	{
+		respuesta=primerNumeroIngresado+segundoNumeroIngresado;
+		if (respuesta<50) 
+		{
+			alert("La suma es "+respuesta+" y es menor a 50");
+		}
+		else
+		{
+			alert("La suma es "+respuesta);
+		}
+		
+	}
 }
