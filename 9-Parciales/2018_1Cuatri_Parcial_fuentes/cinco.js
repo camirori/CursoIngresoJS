45ea790c6aba4102c4a2c5982f9854e04e344714
camirori <you@example.com>
2019-02-17 01:12:15 -0300

    Parcial Ej5 v1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
+	var planetaElegido;
+
+	planetaElegido=prompt("Ingrese un planeta del sistema solar");
+
+	switch(planetaElegido)
+	{
+		case "tierra":
+		{
+			alert("Acá vivimos");
+			break;
+		}
+		case "mercurio":
+		case "venus":
+		{
+			alert("Acá hace más calor");
+			break;
+		}
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+		{
+			alert("Acá hace más frio");
+			break;
+		}
+		default:
+		{
+			alert("No es un planeta válido");
+			break;
+		}
+	}
 
 }
