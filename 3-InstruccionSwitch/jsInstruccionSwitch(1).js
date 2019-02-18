277a80871c495ec2e85a035aa03f3cf7d3e3c152
alumno <alumno>
2019-02-18 19:44:30 -0300

    Ej 3.1 v2

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -6,26 +6,17 @@ function mostrar()
 	switch(mesDelAño) 
 	{
 		case "Enero":
-		{
 			alert("que comiences bien el año!!!.");
 			break;
-		}
 		case "Marzo":
-		{
 			alert("a clases!!!.");
 			break;
-		}
 		case "Julio":
-		{
 			alert("se vienen las vacaciones!!!.");
 			break;
-		}
 		case "Diciembre":
-		{
 			alert("Felices fiesta!!!.");
 			break;
-		}
-
 	}
 
 
