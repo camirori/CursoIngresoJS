db451c56da7989beab1b1fc581b3a1acf1243232
alumno <alumno>
2019-02-18 19:15:53 -0300

    Ej 6.2 v1

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,13 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	var repeticionesDeseadas = prompt("ingrese el número de repeticiones");
+	var repeticiones;
 
+	for(repeticiones=1;repeticiones<=repeticionesDeseadas;repeticiones++)
+	{
+		alert("Hola UTN FRA");
+	}
 
 
 }//FIN DE LA FUNCIÓN
