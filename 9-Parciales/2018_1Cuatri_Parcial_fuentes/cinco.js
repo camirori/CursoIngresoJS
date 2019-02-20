35b846812a7efacd41f1fec0e7c8e71cede17938
alumno <alumno>
2019-02-20 18:50:04 -0300

    Parcial Ej5 v2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -7,30 +7,22 @@ function mostrar()
 	switch(planetaElegido)
 	{
 		case "tierra":
-		{
 			alert("Acá vivimos");
 			break;
-		}
 		case "mercurio":
 		case "venus":
-		{
 			alert("Acá hace más calor");
 			break;
-		}
 		case "marte":
 		case "jupiter":
 		case "saturno":
 		case "urano":
 		case "neptuno":
-		{
 			alert("Acá hace más frio");
 			break;
-		}
 		default:
-		{
 			alert("No es un planeta válido");
 			break;
-		}
 	}
 
 }
