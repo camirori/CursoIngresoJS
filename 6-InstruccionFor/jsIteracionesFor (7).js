6105390528451a5e602ea060e852c2a584baed8d
camirori <you@example.com>
2019-02-19 23:35:02 -0300

    Ej 6.7 v1

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,22 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var contadorDivisores=0;
+	var divisores;
 
+	numeroIngresado=prompt("Ingrese un número");
+	numeroIngresado=parseInt(numeroIngresado);
+
+	for(divisores=1; divisores<=numeroIngresado; divisores++)
+	{
+		if(numeroIngresado%divisores==0)
+		{
+			contadorDivisores++;
+			document.write(divisores+", ");
+		}
+	}
+
+	document.write("cantidad de divisores: "+contadorDivisores);
 
 
 
