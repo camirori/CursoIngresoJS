b8ce17019bdf5cc6e9dbb640f37a82ed6d609eb6
camirori <you@example.com>
2019-02-19 23:43:16 -0300

    Ej 6.8 v1

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,28 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var contadorDivisores=0;
+	var divisores;
 
+	numeroIngresado=prompt("Ingrese un número");
+	numeroIngresado=parseInt(numeroIngresado);
 
+	for(divisores=1; divisores<=numeroIngresado; divisores++)
+	{
+		if(numeroIngresado%divisores==0)
+		{
+			contadorDivisores++;
+		}
+	}
+
+	if(contadorDivisores==2)
+	{
+		alert("El numero ingresado es primo.");
+	}
+	else
+	{
+		alert("El numero ingresado no es primo.");
+	}
 
 
 }//FIN DE LA FUNCIÓN
