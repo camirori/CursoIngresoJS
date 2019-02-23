3c6b4e42e6a1ff8f052d6b477e387ebc788c93f0
camirori <you@example.com>
2019-02-23 14:17:45 -0300

    TP Ej9 v1

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -7,16 +7,54 @@ y una de las cuatro operaciones básicas (suma, resta,
 se debe informar si el resultado es el correcto o no.
 */
 var respuesta;
+var primerNumero;
+var segundoNumero;
+var operador;
+var resultado;
+var respuesta;
 
 
 function comenzar()
 {
+	primerNumero=Math.floor(Math.random()*10)+1;
+	segundoNumero=Math.floor(Math.random()*10)+1;	
+	operador=Math.floor(Math.random()*4)+1;
 
-	
+	PrimerNumero.value=primerNumero;
+	SegundoNumero.value=segundoNumero;
+
+	switch(operador)
+	{
+		case 1:
+			Operador.value="+";
+			resultado=primerNumero+segundoNumero;
+			break;
+		case 2:
+			Operador.value="-";
+			resultado=primerNumero-segundoNumero;
+			break;
+		case 3:
+			Operador.value="*";
+			resultado=primerNumero*segundoNumero;
+			break;
+		case 4:
+			Operador.value="/";
+			resultado=primerNumero/segundoNumero;
+	}
+	console.log(resultado);
+
 
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
+	respuesta=Respuesta.value;
+	if (respuesta==resultado) 
+	{
+		alert("Su respuesta es correcta.");
+	}
+	else{ alert("Su respuesta es incorrecta.");}
 
+	Respuesta.value="";
+	comenzar();
 
 }//FIN DE LA FUNCIÓN
