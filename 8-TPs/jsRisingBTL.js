3a0b3de1a99606ebd3c20439d1d194f86ceac8ae
camirori <you@example.com>
2019-02-23 17:30:04 -0300

    TP Ej12 v1

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,56 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var sueldo;
+	var legajo;
+	var nacionalidad;
+
+	edad=prompt("Ingrese su edad \(entre 18 y 90 inclusive\)");
+	while(edad<18||edad>90)
+	{
+		edad=prompt("La información ingresada no es válida. \nIngrese su edad \(entre 18 y 90 inclusive\)");
+	}
+	Edad.value=edad;
+
+	sexo=prompt("Ingrese su sexo \(\“M\” para masculino y \“F\” para femenino\)");
+	while(sexo!="M"&&sexo!="F")
+	{
+		sexo=prompt("La información ingresada no es válida. \nIngrese su sexo \(\“M\” para masculino y \“F\” para femenino\)");
+	}
+	Sexo.value=sexo;
+
+	estadoCivil=prompt("Ingrese su estadoCivil \(1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos\)");
+	while(estadoCivil<1||estadoCivil>4)
+	{
+		estadoCivil=prompt("La información ingresada no es válida. \n1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos\)");
+	}
+	EstadoCivil.value=estadoCivil;
+
+	sueldo=prompt("Ingrese su sueldo bruto \(no debe ser menor a 8000\)");
+	while(sueldo<8000)
+	{
+		sueldo=prompt("La información ingresada no es válida. \nIngrese su sueldo bruto \(no debe ser menor a 8000\)");
+	}
+	Sueldo.value=sueldo;
+
+	legajo=prompt("Ingrese su número de legajo \(numérico de 4 cifras, sin ceros a la izquierda\)");
+	while(legajo<1000||legajo>9999)
+	{
+		legajo=prompt("La información ingresada no es válida. \nIngrese su número de legajo \(numérico de 4 cifras, sin ceros a la izquierda\)");
+	}
+	Legajo.value=legajo;
+
+	nacionalidad=prompt("Ingrese su nacionalidad \(“A” para argentinos, “E” para extranjeros, “N” para nacionalizados\)");
+	while(nacionalidad!="A"&&nacionalidad!="E"&&nacionalidad!="N")
+	{
+		nacionalidad=prompt("La información ingresada no es válida. \nIngrese su nacionalidad \(“A” para argentinos, “E” para extranjeros, “N” para nacionalizados\)");
+	}
+	Nacionalidad.value=nacionalidad;		
+
+
+
+
 }
