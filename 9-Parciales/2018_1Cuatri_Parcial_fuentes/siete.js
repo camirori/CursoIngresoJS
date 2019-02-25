821a042ba3e81c0541aa978365b0aa9fee312083
alumno <alumno>
2019-02-25 19:23:41 -0300

    Parcial Ej 7 v1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,47 @@
 function mostrar()
 {
+	var notaIngresada;
+	var sexoIngresado;
+	var contadorIngreso;
+	var promedio=0;
+	var notaMinima;
+	var sexoNotaMinima;
+	var contadorMayor=0;
+
+
+	for(contadorIngreso=0;contadorIngreso<5;contadorIngreso++)
+	{
+		notaIngresada=prompt("Ingrese una nota entre 0 y 10");
+		notaIngresada=parseInt(notaIngresada);
+		while(notaIngresada<0||notaIngresada>10)
+		{
+			notaIngresada=prompt("La informacion ingresada no es valida.\nIngrese una nota entre 0 y 10");
+			notaIngresada=parseInt(notaIngresada);
+		}
+
+		sexoIngresado=prompt("Ingrese sexo (“f” o “m”)");
+		while(sexoIngresado!="f"&&sexoIngresado!="m")
+		{
+			sexoIngresado=prompt("La informacion ingresada no es valida.\nIngrese sexo (“f” o “m”)");
+		}
+
+		promedio+=notaIngresada;
+
+		if (notaIngresada<notaMinima||notaMinima==undefined) 
+		{
+			notaMinima=notaIngresada;
+			sexoNotaMinima=sexoIngresado;
+		}
+		if(notaIngresada>5&&sexoIngresado=="m")
+		{
+			contadorMayor++;
+		}
+	}
+
+promedio/=contadorIngreso;
+
+alert("Promedio: "+promedio+". La nota mas baja es "+notaMinima+" y el sexo de esa persona es "+sexoNotaMinima+". La contidad de varones cuya nota ha sido mayor o igual a 6 es "+contadorMayor);
+
+
 
 }
