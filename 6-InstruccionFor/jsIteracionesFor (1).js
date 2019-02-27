93c40cc47e7ec6dd10eabf195e42249bfd641d42
alumno <alumno>
2019-02-27 20:21:49 -0300

    Ejercicio clase random+switch+continue

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,11 +1,84 @@
 function mostrar()
-{
+{	/*
 	var numeros;
 
 	for(numeros=1;numeros<11;numeros++)
 	{
 		document.write(numeros+", ");
 	}
+	*/
 
+	var numeroRandom;
+	var contador;
+	var contador1=0;
+	var contador2=0;
+	var contador3=0;
+	var contador4=0;
+	var contador5=0;
+	var contador6=0;
+	var contador7=0;
+	var contador8=0;
+	var contador9=0;
+	var contador10=0;
+
+
+	for(contador=0;contador<100;contador++)
+	{
+		numeroRandom=Math.floor(Math.random()*10+1);
+		console.log(numeroRandom);
+
+		switch(numeroRandom)
+		{
+			case 1:
+				contador1++;
+				break;
+			case 2:
+				contador2++;
+				break;
+			case 3:
+				contador3++;
+				break;
+			case 4:
+				contador4++;
+				break;
+			case 5:
+				contador5++;
+				break;
+			case 6:
+				contador6++;
+				break;
+			case 7:
+				if(contador7/contador>0.07)
+				{
+					contador--;
+					continue;
+				}
+				contador7++;
+				break;
+			case 8:
+				contador8++;
+				break;
+			case 8:
+				contador8++;
+				break;
+			case 9:
+				contador9++;
+				break;
+			case 10:
+				contador10++;
+				break;
+		}
+	}
+
+	console.log("El 1 salio: "+contador1+" veces, el "+contador1/contador*100+"% de las veces"+
+				"\nEl 2 salio: "+contador2+" veces, el "+contador2/contador*100+"% de las veces"+
+				"\nEl 3 salio: "+contador3+" veces, el "+contador3/contador*100+"% de las veces"+
+				"\nEl 4 salio: "+contador4+" veces, el "+contador4/contador*100+"% de las veces"+
+				"\nEl 5 salio: "+contador5+" veces, el "+contador5/contador*100+"% de las veces"+
+				"\nEl 6 salio: "+contador6+" veces, el "+contador6/contador*100+"% de las veces"+
+				"\nEl 7 salio: "+contador7+" veces, el "+contador7/contador*100+"% de las veces"+
+				"\nEl 8 salio: "+contador8+" veces, el "+contador8/contador*100+"% de las veces"+
+				"\nEl 9 salio: "+contador9+" veces, el "+contador9/contador*100+"% de las veces"+
+				"\nEl 10 salio: "+contador10+" veces, el "+contador10/contador*100+"% de las veces")
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,94 @@
 function mostrar()
 {
+	var animalIngresado;
+	var pesoIngresado;
+	var temperaturaIngresada;
+	var respuesta="si";
+	var contadorTemperaturaPar=0;
+	var contadorTemperaturaMenorCero=0;
+	var acumuladorPeso=0;
+	var contadorAnimales=0;
+	var promedioPeso;
+	var pesoMaximo;
+	var animalPesoMaximo;
+	var temperaturaPesoMaximo;
+	var pesoMaximoTemperaturaMenorCero;
+	var pesoMinimoTemperaturaMenorCero;
 
+
+	while(respuesta=="si")
+	{
+		//Ingreso marca, peso, temperatura
+
+		animalIngresado=prompt("Ingrese el nombre de un animal del zoologico");
+		while(!isNaN(animalIngresado))
+		{
+			animalIngresado=prompt("La informacion ingresada no es valida. Ingrese el nombre de un animal del zoologico");
+		}
+
+		pesoIngresado=prompt("Ingrese el peso del animal, entre 1 y 1000");
+		pesoIngresado=parseInt(pesoIngresado);
+		while(isNaN(pesoIngresado) || pesoIngresado<1||pesoIngresado>1000)
+		{
+			pesoIngresado=prompt("La informacion ingresada no es valida. Ingrese el peso del animal, entre 1 y 1000");
+			pesoIngresado=parseInt(pesoIngresado);
+		}
+
+		temperaturaIngresada=prompt("Ingrese la temperatura del habitat, entre -30 y 30");
+		temperaturaIngresada=parseInt(temperaturaIngresada);
+		while(isNaN(temperaturaIngresada) || temperaturaIngresada<-30||temperaturaIngresada>30)
+		{
+			temperaturaIngresada=prompt("La informacion ingresada no es valida. Ingrese la temperatura del habitat, entre -30 y 30");
+			temperaturaIngresada=parseInt(temperaturaIngresada);
+		}
+
+		respuesta=prompt("Ingrese 'si' para ingresar otro dato");
+
+		// a) La cantidad de temperaturas pares. 
+		if(temperaturaIngresada%2==0)
+		{
+			contadorTemperaturaPar++;
+		}
+
+		//  b) El nombre y temperatura del animal más pesado
+		if(pesoIngresado>pesoMaximo||pesoMaximo==undefined)
+		{
+			pesoMaximo=pesoIngresado;
+			temperaturaPesoMaximo=temperaturaIngresada;
+			animalPesoMaximo=animalIngresado;
+		}	
+		// f) El peso máximo y el mínimo de todos los animales cuyas temperaturas sean bajo cero.
+		if(temperaturaIngresada<0)
+		{
+			if(pesoIngresado>pesoMaximoTemperaturaMenorCero||pesoMaximoTemperaturaMenorCero==undefined)
+			{
+				pesoMaximoTemperaturaMenorCero=pesoIngresado;
+			}
+			if(pesoIngresado<pesoMinimoTemperaturaMenorCero||pesoMinimoTemperaturaMenorCero==undefined)
+			{
+				pesoMinimoTemperaturaMenorCero=pesoIngresado;
+			}
+		}
+		
+
+		// c) La cantidad de animales que viven a menos de 0 grados
+		if(temperaturaIngresada<0)
+		{
+			contadorTemperaturaMenorCero++;
+		}
+
+		//  d) El promedio del peso de todos los animales
+		acumuladorPeso+=pesoIngresado;
+		contadorAnimales++;
+
+	}
+
+	promedioPeso=acumuladorPeso/contadorAnimales;
+
+	document.write("Cantidad de temperaturas pares: "+contadorTemperaturaPar,
+					"<br>Marca del producto más pesado: "+marcaPesoMaximo,
+					"<br>Cantidad de productos que se conservan a menos de 0 grados: "+contadorTemperaturaMenorCero,
+					"<br>Promedio del peso de todos los productos: "+promedioPeso,
+					"<br>Peso máximo: "+pesoMaximo,
+					"<br>Peso minimo: "+pesoMinimo);
 }
